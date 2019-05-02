34b5208f0d943802f6cc3da869a594835821cae1
FELIX CHERUIYOT <kantwk@gmail.com>
2019-05-02 13:29:59 +0300

    Django 2.0 support

--- knowledge/urls.py
+++ knowledge/urls.py
@@ -1,24 +1,26 @@
 from django.urls import path, re_path, include
 
+from .views import knowledge_index, knowledge_list, knowledge_thread, knowledge_moderate, knowledge_ask
+
 urlpatterns = [
     path('', include(([
-        re_path(r'^$', 'knowledge_index', name='knowledge_index'),
+        re_path(r'^$', knowledge_index, name='knowledge_index'),
 
-        re_path(r'^questions/$', 'knowledge_list', name='knowledge_list'),
+        re_path(r'^questions/$', knowledge_list, name='knowledge_list'),
 
         re_path(r'^questions/(?P<question_id>\d+)/$',
-            'knowledge_thread', name='knowledge_thread_no_slug'),
+            knowledge_thread, name='knowledge_thread_no_slug'),
 
-        re_path(r'^questions/(?P<category_slug>[a-z0-9-_]+)/$', 'knowledge_list',
+        re_path(r'^questions/(?P<category_slug>[a-z0-9-_]+)/$', knowledge_list,
             name='knowledge_list_category'),
 
         re_path(r'^questions/(?P<question_id>\d+)/(?P<slug>[a-z0-9-_]+)/$',
-            'knowledge_thread', name='knowledge_thread'),
+            knowledge_thread, name='knowledge_thread'),
 
         re_path(r'^moderate/(?P<model>[a-z]+)/'
             r'(?P<lookup_id>\d+)/(?P<mod>[a-zA-Z0-9_]+)/$',
-            'knowledge_moderate', name='knowledge_moderate'),
+            knowledge_moderate, name='knowledge_moderate'),
 
-        re_path(r'^ask/$', 'knowledge_ask', name='knowledge_ask'),
+        re_path(r'^ask/$', knowledge_ask, name='knowledge_ask'),
     ], 'knowledge'), namespace='knowledge')),
 ]
