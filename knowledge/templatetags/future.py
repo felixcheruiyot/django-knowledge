196de7ab940e20f7d0353c0aefc658587d5684f2
FELIX CHERUIYOT <kantwk@gmail.com>
2019-05-02 13:50:03 +0300

    BF

--- knowledge/templatetags/future.py
+++ knowledge/templatetags/future.py
@@ -4,11 +4,11 @@ from django.template.defaulttags import cycle as cycle_original
 register = Library()
 
 
-@register.tag
+@register.simple_tag
 def cycle(*args, **kwargs):
     ''' A stub to get SortableTabularInline to work '''
     return cycle_original(*args, **kwargs)
 
-@register.tag
+@register.simple_tag
 def url(*args, **kwargs):
     return " "
